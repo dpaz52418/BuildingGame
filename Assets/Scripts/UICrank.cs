44793bd6da5a535974a50e0292ba72a65e485547
Diego Paz <dpaz52418@gmail.com>
2026-03-09 15:31:16 -0700

    crank pt 2 fill image

--- Assets/Scripts/UICrank.cs
+++ Assets/Scripts/UICrank.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class UICrank : MonoBehaviour
 {
     [SerializeField] private float rotationSpeed = 90f; // degrees per second
+    [SerializeField] private Image fillImage; // UI Image (Filled, Radial 360)
 
     private RectTransform rectTransform;
     private int rotationDirection = -1; // -1 = clockwise, 1 = counter-clockwise (Z-axis)
@@ -18,6 +20,13 @@ public class UICrank : MonoBehaviour
     void Update()
     {
         rectTransform.Rotate(0f, 0f, rotationDirection * rotationSpeed * Time.deltaTime);
+
+        if (fillImage != null)
+        {
+            // map the crank's current Z rotation (0-360) to fill amount (0-1)
+            float zAngle = rectTransform.localEulerAngles.z;
+            fillImage.fillAmount = zAngle / 360f;
+        }
     }
 
     public void OnRotateLeft(InputValue value)
